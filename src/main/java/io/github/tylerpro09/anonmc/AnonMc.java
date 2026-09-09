c9058a1b37cbcc26af2ec3e171a3ec0a8d283592
Tylerpro09 <90631485+Tylerpro09@users.noreply.github.com>
2026-09-08 19:09:04 -0500

    Register AnonMC server handshake timeout tick

--- src/main/java/io/github/tylerpro09/anonmc/AnonMc.java
+++ src/main/java/io/github/tylerpro09/anonmc/AnonMc.java
@@ -4,6 +4,7 @@ import io.github.tylerpro09.anonmc.config.AnonConfig;
 import io.github.tylerpro09.anonmc.network.AnonNetworking;
 import io.github.tylerpro09.anonmc.server.ServerAliasManager;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 public final class AnonMc implements ModInitializer {
@@ -15,11 +16,10 @@ public final class AnonMc implements ModInitializer {
         AnonNetworking.registerPayloads();
         AnonNetworking.registerServerReceivers();
 
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            ServerAliasManager.onJoin(handler.getPlayer());
-        });
-        ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
-            ServerAliasManager.onLeave(handler.getPlayer().getUUID());
-        });
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) ->
+                ServerAliasManager.onJoin(handler.getPlayer()));
+        ServerPlayConnectionEvents.DISCONNECT.register((handler, server) ->
+                ServerAliasManager.onLeave(handler.getPlayer().getUUID()));
+        ServerTickEvents.END_SERVER_TICK.register(ServerAliasManager::tick);
     }
 }
